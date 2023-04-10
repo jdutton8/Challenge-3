e54411054fd4e39f9812467cdee2381946c4e25a
Jenny Dutton <jennydutton7@gmail.com>
2023-04-09 22:04:17 -0500

    final commit

--- script.js
+++ script.js
@@ -16,45 +16,54 @@ var lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', '
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbols = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',];
 
-function generatePassword() {
-  var useUppercase;
-  var useLowercase;
-  var useNumber;
-  var useSpecialChar;
 
+
+function generatePassword() {
+
+  var generatedPassword = "";
   var length = prompt("How many characters in your password?");
  var isValidNumber = Number(length) >= 8 && Number(length) <= 128;
  if (!isValidNumber){
   alert("Length must be between 8 and 128 characters");
-  return;
+  return generatedPassword;
  }
 
- if (confirm("Use uppercase?")) {
-  useUppercase = true;
- } else {
-  useUppercase = false;
- }
- if (confirm("Use lowercase?")) {
-  useLowercase = true;
- } else {
-  useLowercase = false;
- }
- if (confirm("Use number?")) {
-  useNumber = true;
- } else {
-  useNumber = false;
- }
- if (confirm("Use special character?")) {
-  useSpecialChar = true;
- } else {
-  useSpecialChar = false;
- }
+ var useUppercase = confirm("Do you want uppercase letters?");
+ 
+ var useLowercase = confirm("Do you want lowercase letters?");
+
+ var useNumber = confirm("Do you want numbers?");
+
+var useSpecialChar = confirm("Do you want special characters?");
+ 
+
+
+var criteriaOptions = [uppers, lowers, numbers, symbols];
+var criteriaFilters = [useUppercase, useLowercase, useNumber, useSpecialChar];
 
 if (useUppercase || useLowercase || useNumber || useSpecialChar) {
+  var passwordCriteria = [];
+  for (i=0; i < criteriaFilters.length; i++) {
+    if (criteriaFilters[i]) {
+      passwordCriteria.push(criteriaOptions[i]);
+    }
+  }
+  for (i = 0; i < length; i++ ) {
+    var selectedCriteria = passwordCriteria[getRandomInt(passwordCriteria.length-1)];
+    var charNumber = getRandomInt(selectedCriteria.length-1);
+  
+    generatedPassword += selectedCriteria[charNumber];
+  }
   
 } else {
   alert("Select at least one character type")
 }
+return generatedPassword;
+}
+
+function getRandomInt(max) {
+  max = Math.floor(max);
+  return Math.floor(Math.random()*max);
 }
 
 // Add event listener to generate button
